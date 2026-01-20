bf8dcf4a78fb32ee39e9b9eb218a573d14e22746
Vinicius Krüger <krugerdv12@gmail.com>
2026-01-20 17:18:40 -0300

    erro

---
+++ jstooling/aula5/src/assets/modules/GeraCPF.js
@@ -0,0 +1,16 @@
+import ValidaCPF from './validaCPF';
+
+export default class GeraCPF{
+    rand(min = 100000000, max = 999999999){
+        return String(Math.floor(Math.random() * (max - min) + min));
+    }
+
+    geraNovoCpf(){
+        const cpfSemDigito = this.rand();
+        const digito1 = ValidaCPF.criaDigito(cpfSemDigito);
+        const digito2 = ValidaCPF.criaDigito(cpfSemDigito + digito1);
+        const novoCpf = cpfSemDigito + digito1 + digito2;
+
+        return novoCpf;
+    }
+}
