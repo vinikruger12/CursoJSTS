d7c86189891ae97ae085ad59e8a862f14e8cf3fb
vinikruger12 <krugerdv12@gmail.com>
2026-02-04 11:30:46 -0300

    aula 7 - reactbase

--- react/react-base/src/App.js
+++ react/react-base/src/App.js
@@ -3,16 +3,18 @@ import React from "react";
 import Login from "./pages/Login";
 import GlobalStyle from "./styles/GlobalStyles";
 
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+
 import Header from "./components/Header";
+import Routes from "./routes";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
+      <Routes/>
       <GlobalStyle />
-      <Login />
-
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ react/react-base/src/pages/Page404/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Container } from "../../styles/GlobalStyles";
+
+
+export default function Page404(){
+    return(
+        <Container>
+        <h1>Pagina nao existe</h1>
+        </Container>
+    )
+};
