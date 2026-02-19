0bd177410da66c4c3ea1af08b8c9ae0bb02dcc85
vinikruger12 <krugerdv12@gmail.com>
2026-02-19 11:28:59 -0300

    isp

---
+++ tsSolid/src/isp/classes/shopping-cart.ts
@@ -0,0 +1,45 @@
+import { Discount } from "./discount";
+import { CartItem } from "./interfaces/car-item";
+
+export class ShoppingCart {
+  private readonly _items: CartItem[] = [];
+
+  private readonly discount: Discount;
+  constructor(discount: Discount){
+    this.discount = discount;
+  }
+
+  addItem(item: CartItem): void {
+    this._items.push(item);
+  }
+
+  removeItem(index: number): void {
+    this._items.splice(index, 1);
+  }
+
+  get items(): Readonly<CartItem[]> {
+    return this._items;
+  }
+
+  total(): number {
+    let total = 0;
+    for (let i = 0; i < this._items.length; i++) {
+      total += this._items[i]?.price ?? 0;
+    }
+    return total;
+  }
+
+  totalWithDiscount():number{
+    return this.discount.calculate(this.total());
+  }
+
+
+  clear(): void {
+    console.log("Carrinho de compras limpo");
+    this._items.length = 0;
+  }
+
+  isEmpty(): boolean {
+    return this._items.length === 0;
+  }
+}
